f108335ae9ab41871f0f22bca34326a58e41ae46
Gabriel Rocha <rochafrgabriel@gmail.com>
2023-12-12 23:34:39 -0300

    BATMAN cause he got no parents

---
+++ cmd/api/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"votey/internal/server"
+)
+
+func main() {
+
+	server := server.NewServer()
+
+	err := server.ListenAndServe()
+	if err != nil {
+		panic("cannot start server")
+	}
+}
